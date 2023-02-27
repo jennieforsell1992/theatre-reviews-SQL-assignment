d1dee1c4368922c7fa959c92bc3d4f68baf7a158
MarcusRei <marcusreineck@gmail.com>
2023-02-27 13:16:05 +0100

    connected users to theatres

--- src/database/populateDB.js
+++ src/database/populateDB.js
@@ -1,32 +1,77 @@
 const { sequelize } = require("./config");
 const { theatres } = require("../data/theatres");
+const { users } = require("../data/users");
 
 const seedTheatresDb = async () => {
   try {
     await sequelize.query(`DROP TABLE IF EXISTS theatre;`);
+    await sequelize.query(`DROP TABLE IF EXISTS user;`);
+
+    // User
+    await sequelize.query(`
+    CREATE TABLE IF NOT EXISTS user (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT NOT NULL,
+      password TEXT NOT NULL,
+      email TEXT NOT NULL,
+      role TEXT NOT NULL
+    );
+    `);
+
+    let userInsertQuery =
+      "INSERT INTO user (username, password, email, role) VALUES ";
+
+    let userInsertQueryVariables = [];
+
+    users.forEach((user, index, array) => {
+      let string = "(";
+      for (let i = 1; i < 5; i++) {
+        string += `$${userInsertQueryVariables.length + i}`;
+        if (i < 4) string += ",";
+      }
+      userInsertQuery += string + ")";
+      if (index < array.length - 1) userInsertQuery += ",";
+
+      const variables = [user.username, user.password, user.email, user.role];
+      userInsertQueryVariables = [...userInsertQueryVariables, ...variables];
+    });
+
+    userInsertQuery += ";";
+
+    await sequelize.query(userInsertQuery, {
+      bind: userInsertQueryVariables,
+    });
+
+    const [userResponse, metadata] = await sequelize.query(
+      "SELECT username, id FROM user"
+    );
+    console.log(userResponse.id);
+    //Theatres
 
     await sequelize.query(`
         CREATE TABLE IF NOT EXISTS theatre (
-          number INTEGER PRIMARY KEY AUTOINCREMENT,
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
           theatreName TEXT NOT NULL,
           address TEXT NOT NULL,
           phoneNumber TEXT NOT NULL,
           desc TEXT,
           email TEXT NOT NULL,
-          reviews TEXT
+          reviews TEXT,
+          fk_user_id INTEGER NOT NULL,
+          FOREIGN KEY(fk_user_id) REFERENCES user(id)
         );
         `);
 
     let theatreInsertQuery =
-      "INSERT INTO theatre (theatreName, address, phoneNumber, desc, email, reviews) VALUES ";
+      "INSERT INTO theatre (theatreName, address, phoneNumber, desc, email, reviews, fk_user_id) VALUES ";
 
     let theatreInsertQueryVariables = [];
 
     theatres.forEach((theatre, index, array) => {
       let string = "(";
-      for (let i = 1; i < 7; i++) {
+      for (let i = 1; i < 8; i++) {
         string += `$${theatreInsertQueryVariables.length + i}`;
-        if (i < 6) string += ",";
+        if (i < 7) string += ",";
       }
       theatreInsertQuery += string + ")";
       if (index < array.length - 1) theatreInsertQuery += ",";
@@ -39,6 +84,13 @@ const seedTheatresDb = async () => {
         theatre.email,
         theatre.reviews,
       ];
+
+      console.log(userResponse);
+      const userId = userResponse.find(
+        (user) => user.username === theatre.owner
+      );
+
+      variables.push(userId.id);
       theatreInsertQueryVariables = [
         ...theatreInsertQueryVariables,
         ...variables,
@@ -51,11 +103,7 @@ const seedTheatresDb = async () => {
       bind: theatreInsertQueryVariables,
     });
 
-    const [theatresResponse, metadata] = await sequelize.query(
-      "SELECT * FROM theatre"
-    );
-
-    //Nästa tabell
+    //Tabell 3
   } catch (error) {
     console.error(error);
   } finally {
