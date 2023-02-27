8fba227f8723c1d6bcde042ae27e1ec020aa0382
MarcusRei <marcusreineck@gmail.com>
2023-02-27 10:49:17 +0100

    Fixed populate fn

---
+++ src/data/theatres.js
@@ -0,0 +1,20 @@
+exports.theatres = [
+  {
+    number: "SVGFEDropShadowElement",
+    theatreName: "Jönköpingsteater",
+    address: "gatan 3",
+    phoneNumber: "03657574",
+    desc: "En teater",
+    email: "teater@email.com",
+    reviews: "Hej Hej",
+  },
+  {
+    number: "2",
+    theatreName: "Stockholmsteatern",
+    address: "gatan 4",
+    phoneNumber: "03657334",
+    desc: "två teater",
+    email: "teaterSthlm@email.com",
+    reviews: "Hej Hej",
+  },
+];
---
+++ src/database/populateDB.js
@@ -0,0 +1,66 @@
+const { sequelize } = require("./config");
+const { theatres } = require("../data/theatres");
+
+const seedTheatresDb = async () => {
+  try {
+    await sequelize.query(`DROP TABLE IF EXISTS theatres;`);
+
+    await sequelize.query(`
+        CREATE TABLE IF NOT EXISTS theatre (
+          number INTEGER PRIMARY KEY AUTOINCREMENT,
+          theatreName TEXT NOT NULL,
+          address TEXT NOT NULL,
+          phoneNumber TEXT NOT NULL,
+          desc TEXT,
+          email TEXT NOT NULL,
+          reviews TEXT
+        );
+        `);
+
+    let theatreInsertQuery =
+      "INSERT INTO theatre (theatreName, address, phoneNumber, desc, email, reviews) VALUES ";
+
+    let theatreInsertQueryVariables = [];
+
+    theatres.forEach((theatre, index, array) => {
+      let string = "(";
+      for (let i = 1; i < 7; i++) {
+        string += `$${theatreInsertQueryVariables.length + i}`;
+        if (i < 6) string += ",";
+      }
+      theatreInsertQuery += string + ")";
+      if (index < array.length - 1) theatreInsertQuery += ",";
+
+      const variables = [
+        theatre.theatreName,
+        theatre.address,
+        theatre.phoneNumber,
+        theatre.desc,
+        theatre.email,
+        theatre.reviews,
+      ];
+      theatreInsertQueryVariables = [
+        ...theatreInsertQueryVariables,
+        ...variables,
+      ];
+    });
+
+    theatreInsertQuery += ";";
+
+    await sequelize.query(theatreInsertQuery, {
+      bind: theatreInsertQueryVariables,
+    });
+
+    const [theatresResponse, metadata] = await sequelize.query(
+      "SELECT * FROM theatre"
+    );
+
+    //Nästa tabell
+  } catch (error) {
+    console.error(error);
+  } finally {
+    process.exit(0);
+  }
+};
+
+seedTheatresDb();
