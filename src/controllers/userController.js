a1bb8387292ad8318f54856572bd9032afeae876
Wilma Wiker <wikerwilma@gmail.com>
2023-02-28 11:13:07 +0100

    modified controllers

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -44,11 +44,17 @@ exports.createNewUser = async (req, res) => {
 };
 
 exports.updateUser = async (req, res) => {
-  const user = req.body.user;
   const userId = req.params.userId || req.body.userId;
 
+  const { username, password, email, role } = req.body;
+
+  if (username) username = username;
+  if (password) password = password;
+  if (email) email = email;
+  if (role) role = role;
+
   const [updatedUserId] = await sequelize.query(
-    `UPDATE user SET  WHERE id = userId RETURNING *`,
+    `UPDATE user SET (username = $username, password = $password, email = $email, role = $role) WHERE id = $userId RETURNING *`,
     {
       bind: { user: user, userId: userId },
       type: QueryTypes.UPDATE,
