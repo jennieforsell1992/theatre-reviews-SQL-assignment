7457d0e8862d14e9fd636d168f06d787cb834216
jennie Forsell <forselljennie@gmail.com>
2023-02-28 10:59:50 +0100

    userRoutes done

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const { userRoles } = require("../constants/users");
 const router = express.Router();
+const {
+  getAllUsers,
+  getUserById,
+  createNewUser,
+  updateUser,
+  deleteUserById,
+} = require("../controllers/userController");
 
-router.get("/");
-router.get("/userId");
-router.post("/");
-router.put("/userId");
-router.delete("/userId");
+router.get("/", getAllUsers);
+router.get("/userId", getUserById);
+router.post("/", createNewUser);
+router.put("/userId", updateUser);
+router.delete("/userId", deleteUserById);
