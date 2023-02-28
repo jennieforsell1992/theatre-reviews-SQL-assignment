58899cc8060f9c2b8cff8aedb14cb188da72f871
MarcusRei <marcusreineck@gmail.com>
2023-02-28 16:10:42 +0100

    continued work on login controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,13 @@
 const { sequelize } = require("../database/config");
 const { QueryTypes } = require("sequelize");
-const { UnauthorizedError, BadRequestError } = require("../utils/errors");
+const {
+  UnauthorizedError,
+  BadRequestError,
+  UnauthenticatedError,
+} = require("../utils/errors");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const userRoles = require("../constants/users");
 
 exports.register = async (req, res) => {
   const { username, password, email, role } = req.body;
@@ -37,4 +43,50 @@ exports.register = async (req, res) => {
   return res.status(201).json({
     message: "Registration succeeded. You can now log in.",
   });
+};
+
+exports.login = async (req, res) => {
+  const { email, password: candidatePassword } = req.body;
+
+  if (!email) {
+    throw new BadRequestError("You must provide an email and a password!");
+  }
+
+  //Check if email in DB
+  const [user, metadata] = await sequelize.query(
+    "SELECT * FROM user WHERE email = $email LIMIT 1;",
+    {
+      bind: { email },
+      type: QueryTypes.SELECT,
+    }
+  );
+
+  console.log(user);
+
+  if (!user) throw new UnauthenticatedError("Invalid Credentials");
+
+  //Check if password is correct
+
+  const isPasswordCorrect = await bcrypt.compare(
+    candidatePassword,
+    user.password
+  );
+
+  if (!isPasswordCorrect) throw new UnauthenticatedError("Invalid Credentials");
+
+  //Create JWT payload
+  const jwtPayload = {
+    userId: user.id,
+    username: user.username,
+    email: user.email,
+    role: user["role"] === "ADMIN" ? userRoles.ADMIN : userRoles.USER,
+  };
+
+  const jwtToken = jwt.sign(jwtPayload, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+
+  return res.json({ token: jwtToken, user: jwtPayload });
+
+  return res.send("Du är inloggad!");
 };
